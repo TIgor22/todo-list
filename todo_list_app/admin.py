d9e5d9a578e0bc05fbb57dea972c58d20b3b90af
Ihor Tutak <igor2tak2212@gmail.com>
2024-07-02 12:31:19 +0300

    "register models Task and Tag in admin.py"

--- todo_list_app/admin.py
+++ todo_list_app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from todo_list_app.models import Task, Tag
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["content", "created", "deadline", "is_completed"]
+    list_filter = ["is_completed"]
+    search_fields = ["content"]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    search_fields = ["name"]
