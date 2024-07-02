aadc6ebf4501ea8ff4fa13b566e8129a4c60db77
Ihor Tutak <igor2tak2212@gmail.com>
2024-07-02 13:56:57 +0300

    "add classes TagCreateView, TagUpdateView and TagDeleteView in views.py and path in urls.py"

--- todo_list_app/urls.py
+++ todo_list_app/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from todo_list_app.views import TaskListView, TagListView
+from todo_list_app.views import TaskListView, TagListView, TagCreateView, TagUpdateView, TagDeleteView
 
 app_name = "todo_list_app"
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
     path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/update/<int:pk>/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/delete/<int:pk>/", TagDeleteView.as_view(), name="tag-delete"),
 ]
--- todo_list_app/views.py
+++ todo_list_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from todo_list_app.models import Tag, Task
@@ -7,6 +8,24 @@ from todo_list_app.models import Tag, Task
 class TagListView(generic.ListView):
     model = Tag
     template_name = "todo_list/tag_list.html"
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    template_name = "todo_list/tag_form.html"
+    success_url = reverse_lazy("todo_list_app:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    template_name = "todo_list/tag_form.html"
+    success_url = reverse_lazy("todo_list_app:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    template_name = "todo_list/tag_confirm_delete.html"
+    success_url = reverse_lazy("todo_list_app:tag-list")
 
 
 class TaskListView(generic.ListView):
